0f6f6d15459efd6e2e3f12a62e3e89fccc682c30
sagarpagad <sagarpagad123@gmail.com>
2022-06-18 02:27:07 +0530

    ghghghgh

--- block-chain/src/views/HomeView.vue
+++ block-chain/src/views/HomeView.vue
@@ -4,28 +4,26 @@ import { user } from "../store/user";
 </script>
 
 <template>
-  <div class="relative container mx-auto m-4 bg-blue-900">
+  <div class="fixed inset-x-2 inset-y-5 bg-blue-900">
     <img
-      class="w-full opacity-50"
+      class="w-full h-full opacity-50"
       src="https://images.edexlive.com/uploads/user/imagelibrary/2019/5/22/w600X390/msrit-campus.jpg"
       alt=""
     />
+
     <div class="absolute top-0 flex justify-center items-center w-full h-full">
-      <div class="space-y-25">
-        <div class="text-center text-white font-bold">
-          <p class="text-2xl text-white font-extrabold">
-            E-Voting With Block-Chain
-          </p>
+      <div class="flex flex-col justify-around h-full">
+        <div class="text-center text-white font-bold space-y-4 p-4">
+          <p class="text-white font-extrabold">E-Voting With Block-Chain</p>
           <p>Project On " Block-Chain Based E-Voting System ".</p>
           <p>Guided by DR. Rajendra Prasad P</p>
           <p>This is Created by MSRIT Students</p>
         </div>
-
         <div class="flex justify-center space-x-2">
           <div class="text-center">
             <RouterLink to="/about" class="btn btn-blue">about</RouterLink>
           </div>
-          <div  class="text-center">
+          <div class="text-center">
             <RouterLink to="/user" class="btn btn-green">user</RouterLink>
           </div>
           <div v-if="user.state.role == 'Admin'" class="text-center">
